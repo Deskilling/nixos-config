89be1034ad7a985fa3e5fd595df4b374a980fee9
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-03-14 00:02:46 +0100

    recusive adding dotfiles

--- home/leif/default.nix
+++ home/leif/default.nix
@@ -4,20 +4,17 @@
   home-manager.useUserPackages = true;
   home-manager.users.${username} =
     { config, lib, ... }:
-    let
-      dotfilesPath = "/etc/nixos/home/${username}/dotfiles";
-      link = name: config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/${name}";
-    in
     {
       imports = lib.filesystem.listFilesRecursive ./apps;
       home.username = username;
       home.homeDirectory = "/home/${username}";
       home.stateVersion = "25.11";
-      home.file = builtins.listToAttrs (
-        map (name: {
-          name = ".config/${name}";
-          value.source = link name;
-        }) (builtins.attrNames (builtins.readDir ./dotfiles))
-      );
+      home.file = lib.pipe (lib.filesystem.listFilesRecursive ./dotfiles) [
+        (map (f: {
+          name = ".config/${lib.removePrefix (toString ./dotfiles + "/") (toString f)}";
+          value.source = config.lib.file.mkOutOfStoreSymlink (toString f);
+        }))
+        builtins.listToAttrs
+      ];
     };
 }
