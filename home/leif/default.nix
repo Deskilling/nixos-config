2267f51c1df2abebc139b1628fe0055c948bc4ff
Leif Haberecht <81562562+Deskilling@users.noreply.github.com>
2026-03-14 18:19:36 +0100

    improve dotfiles get

--- home/leif/default.nix
+++ home/leif/default.nix
@@ -9,12 +9,16 @@
       home.username = username;
       home.homeDirectory = "/home/${username}";
       home.stateVersion = "25.11";
-      home.file = lib.pipe (lib.filesystem.listFilesRecursive ./dotfiles) [
-        (map (f: {
+      home.file = builtins.listToAttrs (
+        map (f: {
           name = ".config/${lib.removePrefix (toString ./dotfiles + "/") (toString f)}";
-          value.source = config.lib.file.mkOutOfStoreSymlink (toString f);
-        }))
-        builtins.listToAttrs
-      ];
+          value = {
+            source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/${username}/dotfiles/${
+              lib.removePrefix (toString ./dotfiles + "/") (toString f)
+            }";
+            force = true;
+          };
+        }) (lib.filesystem.listFilesRecursive ./dotfiles)
+      );
     };
 }
